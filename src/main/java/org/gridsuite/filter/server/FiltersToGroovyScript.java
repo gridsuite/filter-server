e9a02d4fd6e7bd2f38d4a731383e0885db681770
Franck LECUYER <franck.lecuyer@rte-france.com>
2021-08-05 13:01:50 +0200

    Removing some code smells
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/filter/server/FiltersToGroovyScript.java
+++ src/main/java/org/gridsuite/filter/server/FiltersToGroovyScript.java
@@ -32,6 +32,13 @@ import static java.util.stream.Collectors.joining;
  */
 @Service
 public class FiltersToGroovyScript {
+    private static final String COLLECTION_NAME = "collectionName";
+    private static final String NO_EMPTY_FILTER = "noEmptyFilter";
+    private static final String EQUIPMENT_ID = "equipmentId";
+    private static final String EQUIPMENT_NAME = "equipmentName";
+    private static final String COUNTRIES = "countries";
+    private static final String SUBSTATION_NAME = "substationName";
+
     private final String lineTemplate;
     private final String injectionTemplate;
     private final String twoWindingsTransformerTemplate;
@@ -108,21 +115,21 @@ public class FiltersToGroovyScript {
         switch (filter.getType()) {
             case LINE:
                 LineFilter lineFilter = (LineFilter) filter;
-                template.add("collectionName", equipmentsCollection);
+                template.add(COLLECTION_NAME, equipmentsCollection);
                 if (!lineFilter.isEmpty()) {
-                    template.add("noEmptyFilter", "true");
+                    template.add(NO_EMPTY_FILTER, "true");
                 }
                 if (lineFilter.getEquipmentID() != null) {
-                    template.add("equipmentId", lineFilter.getEquipmentID());
+                    template.add(EQUIPMENT_ID, lineFilter.getEquipmentID());
                 }
                 if (lineFilter.getEquipmentName() != null) {
-                    template.add("equipmentName", lineFilter.getEquipmentName());
+                    template.add(EQUIPMENT_NAME, lineFilter.getEquipmentName());
                 }
                 if (!CollectionUtils.isEmpty(lineFilter.getCountries1())) {
-                    template.add("countries1", lineFilter.getCountries1().stream().collect(joining("','", "['", "']")));
+                    template.add(COUNTRIES + "1", lineFilter.getCountries1().stream().collect(joining("','", "['", "']")));
                 }
                 if (!CollectionUtils.isEmpty(lineFilter.getCountries2())) {
-                    template.add("countries2", lineFilter.getCountries2().stream().collect(joining("','", "['", "']")));
+                    template.add(COUNTRIES + "2", lineFilter.getCountries2().stream().collect(joining("','", "['", "']")));
                 }
                 if (lineFilter.getNominalVoltage1() != null) {
                     addFilterNominalVoltage(template, lineFilter.getNominalVoltage1(), "1");
@@ -131,27 +138,27 @@ public class FiltersToGroovyScript {
                     addFilterNominalVoltage(template, lineFilter.getNominalVoltage2(), "2");
                 }
                 if (!StringUtils.isEmpty(lineFilter.getSubstationName1())) {
-                    template.add("substationName1", lineFilter.getSubstationName1());
+                    template.add(SUBSTATION_NAME + "1", lineFilter.getSubstationName1());
                 }
                 if (!StringUtils.isEmpty(lineFilter.getSubstationName2())) {
-                    template.add("substationName2", lineFilter.getSubstationName2());
+                    template.add(SUBSTATION_NAME + "2", lineFilter.getSubstationName2());
                 }
                 break;
 
             case TWO_WINDINGS_TRANSFORMER:
                 TwoWindingsTransformerFilter twoWindingsTransformerFilter = (TwoWindingsTransformerFilter) filter;
-                template.add("collectionName", equipmentsCollection);
+                template.add(COLLECTION_NAME, equipmentsCollection);
                 if (!twoWindingsTransformerFilter.isEmpty()) {
-                    template.add("noEmptyFilter", "true");
+                    template.add(NO_EMPTY_FILTER, "true");
                 }
                 if (twoWindingsTransformerFilter.getEquipmentID() != null) {
-                    template.add("equipmentId", twoWindingsTransformerFilter.getEquipmentID());
+                    template.add(EQUIPMENT_ID, twoWindingsTransformerFilter.getEquipmentID());
                 }
                 if (twoWindingsTransformerFilter.getEquipmentName() != null) {
-                    template.add("equipmentName", twoWindingsTransformerFilter.getEquipmentName());
+                    template.add(EQUIPMENT_NAME, twoWindingsTransformerFilter.getEquipmentName());
                 }
                 if (!CollectionUtils.isEmpty(twoWindingsTransformerFilter.getCountries())) {
-                    template.add("countries", twoWindingsTransformerFilter.getCountries().stream().collect(joining("','", "['", "']")));
+                    template.add(COUNTRIES, twoWindingsTransformerFilter.getCountries().stream().collect(joining("','", "['", "']")));
                 }
                 if (twoWindingsTransformerFilter.getNominalVoltage1() != null) {
                     addFilterNominalVoltage(template, twoWindingsTransformerFilter.getNominalVoltage1(), "1");
@@ -160,24 +167,24 @@ public class FiltersToGroovyScript {
                     addFilterNominalVoltage(template, twoWindingsTransformerFilter.getNominalVoltage2(), "2");
                 }
                 if (!StringUtils.isEmpty(twoWindingsTransformerFilter.getSubstationName())) {
-                    template.add("substationName", twoWindingsTransformerFilter.getSubstationName());
+                    template.add(SUBSTATION_NAME, twoWindingsTransformerFilter.getSubstationName());
                 }
                 break;
 
             case THREE_WINDINGS_TRANSFORMER:
                 ThreeWindingsTransformerFilter threeWindingsTransformerFilter = (ThreeWindingsTransformerFilter) filter;
-                template.add("collectionName", equipmentsCollection);
+                template.add(COLLECTION_NAME, equipmentsCollection);
                 if (!threeWindingsTransformerFilter.isEmpty()) {
-                    template.add("noEmptyFilter", "true");
+                    template.add(NO_EMPTY_FILTER, "true");
                 }
                 if (threeWindingsTransformerFilter.getEquipmentID() != null) {
-                    template.add("equipmentId", threeWindingsTransformerFilter.getEquipmentID());
+                    template.add(EQUIPMENT_ID, threeWindingsTransformerFilter.getEquipmentID());
                 }
                 if (threeWindingsTransformerFilter.getEquipmentName() != null) {
-                    template.add("equipmentName", threeWindingsTransformerFilter.getEquipmentName());
+                    template.add(EQUIPMENT_NAME, threeWindingsTransformerFilter.getEquipmentName());
                 }
                 if (!CollectionUtils.isEmpty(threeWindingsTransformerFilter.getCountries())) {
-                    template.add("countries", threeWindingsTransformerFilter.getCountries().stream().collect(joining("','", "['", "']")));
+                    template.add(COUNTRIES, threeWindingsTransformerFilter.getCountries().stream().collect(joining("','", "['", "']")));
                 }
                 if (threeWindingsTransformerFilter.getNominalVoltage1() != null) {
                     addFilterNominalVoltage(template, threeWindingsTransformerFilter.getNominalVoltage1(), "1");
@@ -189,7 +196,7 @@ public class FiltersToGroovyScript {
                     addFilterNominalVoltage(template, threeWindingsTransformerFilter.getNominalVoltage3(), "3");
                 }
                 if (!StringUtils.isEmpty(threeWindingsTransformerFilter.getSubstationName())) {
-                    template.add("substationName", threeWindingsTransformerFilter.getSubstationName());
+                    template.add(SUBSTATION_NAME, threeWindingsTransformerFilter.getSubstationName());
                 }
                 break;
 
@@ -203,53 +210,53 @@ public class FiltersToGroovyScript {
             case LCC_CONVERTER_STATION:
             case VSC_CONVERTER_STATION:
                 AbstractInjectionFilter injectionFilter = (AbstractInjectionFilter) filter;
-                template.add("collectionName", equipmentsCollection);
+                template.add(COLLECTION_NAME, equipmentsCollection);
                 if (!injectionFilter.isEmpty()) {
-                    template.add("noEmptyFilter", "true");
+                    template.add(NO_EMPTY_FILTER, "true");
                 }
                 if (injectionFilter.getEquipmentID() != null) {
-                    template.add("equipmentId", injectionFilter.getEquipmentID());
+                    template.add(EQUIPMENT_ID, injectionFilter.getEquipmentID());
                 }
                 if (injectionFilter.getEquipmentName() != null) {
-                    template.add("equipmentName", injectionFilter.getEquipmentName());
+                    template.add(EQUIPMENT_NAME, injectionFilter.getEquipmentName());
                 }
                 if (!CollectionUtils.isEmpty(injectionFilter.getCountries())) {
-                    template.add("countries", injectionFilter.getCountries().stream().collect(joining("','", "['", "']")));
+                    template.add(COUNTRIES, injectionFilter.getCountries().stream().collect(joining("','", "['", "']")));
                 }
                 if (injectionFilter.getNominalVoltage() != null) {
                     addFilterNominalVoltage(template, injectionFilter.getNominalVoltage(), null);
                 }
                 if (!StringUtils.isEmpty(injectionFilter.getSubstationName())) {
-                    template.add("substationName", injectionFilter.getSubstationName());
+                    template.add(SUBSTATION_NAME, injectionFilter.getSubstationName());
                 }
                 break;
 
             case HVDC_LINE:
                 HvdcLineFilter hvdcLineFilter = (HvdcLineFilter) filter;
-                template.add("collectionName", equipmentsCollection);
+                template.add(COLLECTION_NAME, equipmentsCollection);
                 if (!hvdcLineFilter.isEmpty()) {
-                    template.add("noEmptyFilter", "true");
+                    template.add(NO_EMPTY_FILTER, "true");
                 }
                 if (hvdcLineFilter.getEquipmentID() != null) {
-                    template.add("equipmentId", hvdcLineFilter.getEquipmentID());
+                    template.add(EQUIPMENT_ID, hvdcLineFilter.getEquipmentID());
                 }
                 if (hvdcLineFilter.getEquipmentName() != null) {
-                    template.add("equipmentName", hvdcLineFilter.getEquipmentName());
+                    template.add(EQUIPMENT_NAME, hvdcLineFilter.getEquipmentName());
                 }
                 if (!CollectionUtils.isEmpty(hvdcLineFilter.getCountries1())) {
-                    template.add("countries1", hvdcLineFilter.getCountries1().stream().collect(joining("','", "['", "']")));
+                    template.add(COUNTRIES + "1", hvdcLineFilter.getCountries1().stream().collect(joining("','", "['", "']")));
                 }
                 if (!CollectionUtils.isEmpty(hvdcLineFilter.getCountries2())) {
-                    template.add("countries2", hvdcLineFilter.getCountries2().stream().collect(joining("','", "['", "']")));
+                    template.add(COUNTRIES + "2", hvdcLineFilter.getCountries2().stream().collect(joining("','", "['", "']")));
                 }
                 if (hvdcLineFilter.getNominalVoltage() != null) {
                     addFilterNominalVoltage(template, hvdcLineFilter.getNominalVoltage(), null);
                 }
                 if (!StringUtils.isEmpty(hvdcLineFilter.getSubstationName1())) {
-                    template.add("substationName1", hvdcLineFilter.getSubstationName1());
+                    template.add(SUBSTATION_NAME + "1", hvdcLineFilter.getSubstationName1());
                 }
                 if (!StringUtils.isEmpty(hvdcLineFilter.getSubstationName2())) {
-                    template.add("substationName2", hvdcLineFilter.getSubstationName2());
+                    template.add(SUBSTATION_NAME + "2", hvdcLineFilter.getSubstationName2());
                 }
                 break;
 
