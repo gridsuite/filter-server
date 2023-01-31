9a915a63fae4e44852b6d9fa723bb392078d90c0
Laurent Garnier <laurent.garnier@rte-france.com>
2023-01-31 08:40:49 +0100

    fix:forgotten source files

---
+++ src/main/java/org/gridsuite/filter/server/entities/FreePropertiesFilterEntity.java
@@ -0,0 +1,43 @@
+/**
+ *  Copyright (c) 2022, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.filter.server.entities;
+
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author Laurent Garnier <laurent.garnier at rte-france.com>
+ */
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "free_properties")
+public class FreePropertiesFilterEntity {
+    @Id
+    @GeneratedValue(strategy  =  GenerationType.AUTO)
+    @Column(name = "id")
+    UUID id;
+
+    //@OneToMany(fetch = FetchType.LAZY, mappedBy = "freePropertiesFilterEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    Set<FreePropertyFilterEntity> freePropertyFilterEntities;
+}
---
+++ src/main/java/org/gridsuite/filter/server/entities/FreePropertyFilterEntity.java
@@ -0,0 +1,53 @@
+/**
+ *  Copyright (c) 2022, RTE (http://www.rte-france.com)
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.gridsuite.filter.server.entities;
+
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author Laurent Garnier <laurent.garnier at rte-france.com>
+ */
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "free_property")
+public class FreePropertyFilterEntity {
+    @Id
+    @GeneratedValue(strategy  =  GenerationType.AUTO)
+    @Column(name = "id")
+    UUID id;
+
+    @Column(name = "propName", nullable = false)
+    String propName;
+
+    @Column(name = "prop_values")
+    @ElementCollection
+    //@CollectionTable(foreignKey = @ForeignKey(), name = "prop_value", joinColumns = @JoinColumn(name = "set_id"))
+    @CollectionTable(name = "prop_value")
+    Set<String> propValues;
+
+    //@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    //@JoinColumn(name = "properties_set_id")
+    //@Setter
+    //FreePropertiesFilterEntity freePropertiesFilterEntity;
+}
