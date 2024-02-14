18125fa7748540f291abc0e67f747cc95f51698c
Hugo Marcellin <hugo.marcelin@rte-france.com>
2024-02-14 16:21:22 +0100

    Checktyle Signed-off-by: Hugo Marcellin <hugo.marcelin@rte-france.com>

--- src/test/java/org/gridsuite/filter/server/FilterRepositoryTest.java
+++ src/test/java/org/gridsuite/filter/server/FilterRepositoryTest.java
@@ -26,9 +26,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.*;
 
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
 
 /**
  * @author Hugo Marcellin <hugo.marcelin at rte-france.com>
