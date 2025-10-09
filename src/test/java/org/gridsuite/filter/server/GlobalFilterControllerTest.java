4f3d418a58c0f9c91f31af4e09b19be13465e0af
Franck LECUYER <franck.lecuyer@rte-france.com>
2025-10-09 16:14:52 +0200

    Add missing GlobalFilterController unit test
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

---
+++ src/test/java/org/gridsuite/filter/server/GlobalFilterControllerTest.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.filter.server;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.spi.json.JacksonJsonProvider;
+import com.jayway.jsonpath.spi.json.JsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import com.jayway.jsonpath.spi.mapper.MappingProvider;
+import com.powsybl.iidm.network.Country;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+import com.powsybl.network.store.client.NetworkStoreService;
+import com.powsybl.network.store.client.PreloadingStrategy;
+import com.powsybl.network.store.iidm.impl.NetworkFactoryImpl;
+import org.gridsuite.filter.globalfilter.GlobalFilter;
+import org.gridsuite.filter.utils.EquipmentType;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import static com.powsybl.iidm.network.VariantManagerConstants.INITIAL_VARIANT_ID;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = {FilterApplication.class, TestChannelBinderConfiguration.class})
+public class GlobalFilterControllerTest {
+    public static final String URL_TEMPLATE = "/" + FilterApi.API_VERSION + "/global-filter";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockBean
+    private NetworkStoreService networkStoreService;
+
+    private static final UUID NETWORK_UUID = UUID.fromString("7928181c-7977-4592-ba19-88027e4254e4");
+
+    @Before
+    public void setUp() {
+        Configuration.defaultConfiguration();
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(DeserializationFeature.USE_LONG_FOR_INTS);
+        mapper.enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS);
+        mapper.disable(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE);
+
+        Network network = EurostagTutorialExample1Factory.createWithMoreGenerators(new NetworkFactoryImpl());
+        network.getSubstation("P1").setProperty("region", "north");
+        network.getSubstation("P1").setName("P1");
+        network.getSubstation("P2").setProperty("region", "south");
+        network.getGenerator("GEN").setProperty("region", "north");
+        network.getGenerator("GEN").setName("GEN");
+        network.getGenerator("GEN2").setProperty("region", "south");
+        network.getLoad("LOAD").setProperty("region", "north");
+        network.getVoltageLevel("VLGEN").setName("VLGEN");
+        given(networkStoreService.getNetwork(NETWORK_UUID, PreloadingStrategy.COLLECTION)).willReturn(network);
+
+        Configuration.setDefaults(new Configuration.Defaults() {
+
+            private final JsonProvider jsonProvider = new JacksonJsonProvider(mapper);
+            private final MappingProvider mappingProvider = new JacksonMappingProvider(mapper);
+
+            @Override
+            public JsonProvider jsonProvider() {
+                return jsonProvider;
+            }
+
+            @Override
+            public MappingProvider mappingProvider() {
+                return mappingProvider;
+            }
+
+            @Override
+            public Set<Option> options() {
+                return EnumSet.noneOf(Option.class);
+            }
+        });
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testEvaluateGlobalFilter() throws Exception {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("networkUuid", NETWORK_UUID.toString());
+        params.add("variantId", INITIAL_VARIANT_ID);
+        params.addAll("equipmentTypes", List.of(EquipmentType.GENERATOR.name()));
+        GlobalFilter globalFilter = new GlobalFilter(List.of("24"), List.of(Country.FR, Country.BE), List.of(), Map.of());
+
+        String expectedJson = objectMapper.writeValueAsString(List.of("GEN", "GEN2"));
+        mvc.perform(post(URL_TEMPLATE)
+                .params(params)
+                .content(objectMapper.writeValueAsString(globalFilter))
+                .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+            .andExpect(content().contentTypeCompatibleWith(APPLICATION_JSON))
+            .andExpect(content().json(expectedJson));
+    }
+}
